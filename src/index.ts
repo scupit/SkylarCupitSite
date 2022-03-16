88148aa7fb7e173605f59f2864ff4a303d99e526
Skylar Cupit <Skylarcupit@gmail.com>
2022-03-16 15:41:57 -0500

    Fixed and improved a whole bunch of styling/layout issues

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,5 @@
 // Each TS file acts as a Webpack "entrypoint". This imports SCSS into the entrypoint,
 // where it is processed by the SCSS and CSS loaders and embedded into an HTML file which
 // is configured to use this entrypoint.
-import "index.scss";
+import "home-page.scss";
 import "../index.mustache"
-
-console.log("Loaded Index!");
