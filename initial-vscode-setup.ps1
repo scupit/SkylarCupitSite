f575a7d6bb710ccc6198a2e536470d067b387895
Skylar Cupit <Skylarcupit@gmail.com>
2022-12-13 21:27:33 -0600

    Added resume as a submodule

---
+++ initial-vscode-setup.ps1
@@ -0,0 +1,9 @@
+yarn install
+yarn plugin import typescript
+yarn sdks vscode
+
+Set-Location .\resume
+yarn install
+yarn plugin import typescript
+yarn sdks vscode
+Set-Location ..
---
+++ initial-vscode-setup.sh
@@ -0,0 +1,9 @@
+yarn install
+yarn plugin import typescript
+yarn sdks vscode
+
+cd ./resume
+yarn install
+yarn plugin import typescript
+yarn sdks vscode
+cd ..
