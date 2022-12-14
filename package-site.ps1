6f16393aa81a9bd0c3e87bb27211b70357bab337
Skylar Cupit <Skylarcupit@gmail.com>
2022-12-13 21:54:43 -0600

    Site packager scripts now package the resume page and generated resume PDF as well

--- package-site.ps1
+++ package-site.ps1
@@ -1,4 +1,15 @@
 yarn run prod
+
+Set-Location resume
+.\package-resume.ps1
+Set-Location ..
+
+if (Test-Path .\dist\resume) {
+  Remove-Item -Recurse .\dist\resume
+}
+
+Copy-Item -Recurse -Force .\resume\dist .\dist\resume
+
 7z a -ttar my_site.tar dist
 7z a -tgzip my_site.tar.gz my_site.tar
 Remove-Item my_site.tar
--- package-site.sh
+++ package-site.sh
@@ -1,4 +1,12 @@
 yarn run prod
+
+cd resume
+package-resume.sh
+cd ..
+
+rm -r dist/resume
+cp -r resume/dist dist/resume
+
 7z a -ttar my_site.tar dist
 7z a -tgzip my_site.tar.gz my_site.tar
 rm my_site.tar
