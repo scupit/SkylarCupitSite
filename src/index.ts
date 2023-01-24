75a69b068cf86cf3de5fcfec459935a0c93d8d6c
Skylar Cupit <Skylarcupit@gmail.com>
2023-01-24 16:17:22 -0600

    Colors are now set using CSS variables. As a result, themes can now be toggled using JS.

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,30 @@
 // is configured to use this entrypoint.
 import "home-page.scss";
 import "../index.mustache"
+
+let paletteIndex = 0;
+
+function selectPalette(index: number) {
+  const docRoot: HTMLElement = document.documentElement;
+  const propertyList: string[] = [
+    "primary",
+    "secondary",
+    "contact-box-color",
+    "card-bg",
+    "contrast-bg",
+    "contrast-bg-dark",
+    "accent",
+    "dark-accent",
+    "darkest-accent",
+  ];
+
+  for (const propertyName of propertyList) {
+    docRoot.style.setProperty(`--${propertyName}`, `var(--${propertyName}${index})`);
+  }
+}
+
+document.getElementById("site-options")!
+  .addEventListener("click", () => {
+    paletteIndex = (paletteIndex + 1) % 3;
+    selectPalette(paletteIndex);
+  })
--- src/to-learn/to-learn.ts
+++ src/to-learn/to-learn.ts
@@ -1,2 +1,3 @@
 import "./to-learn.scss";
 import "./to-learn.mustache"
+import "index";
